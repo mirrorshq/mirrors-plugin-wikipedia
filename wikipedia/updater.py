def9e91218a4b4569776b3f6ab4d7970ece617d7
fpemud <fpemud@sina.com>
2020-12-14 01:17:32 +0800

    fix

--- wikipedia/updater.py
+++ wikipedia/updater.py
@@ -18,134 +18,18 @@ import mirrors.plugin
 
 def main():
     with mirrors.plugin.ApiClient() as sock:
-        runMode = json.loads(sys.argv[1])["run-mode"]
         dataDir = json.loads(sys.argv[1])["storage-file"]["data-directory"]
 
-        if runMode == "init":
-            _init(dataDir, sock)
-        elif runMode == "update":
-            _update(dataDir, sock)
-        else:
-            assert False
+        dumpsDataDir = os.path.join(dataDir, "dumps")
+        dumpsRsyncSource = "rsync://ftp.acc.umu.se/mirror/wikimedia.org/dumps"
+        _Util.cmdExec("/usr/bin/rsync", "-v", "-a", "-z", "-H", "--delete", dumpsRsyncSource, dumpsDataDir)
 
-
-def _init(dataDir, sock):
-    # find downloaded tar data file
-    dstFile = None
-    dstMd5File = None
-    if True:
-        tlist = glob.glob(os.path.join(dataDir, "*.tar"))
-        tlist = [x for x in tlist if os.path.exists(x + ".md5")]
-        if len(tlist) > 0:
-            dstFile = tlist[-1]
-            dstMd5File = dstFile + ".md5"
-            _Util.deleteDirContent(dataDir, [dstFile, dstMd5File])
-        else:
-            _Util.deleteDirContent(dataDir)
-
-    # check tar data file, download if needed
-    if dstFile is None or not _Util.verifyFile(dstFile, dstMd5File):
-        # clear history
-        dstFile = None
-        dstFileUrl = None
-        _Util.deleteDirContent(dataDir)
-
-        # get tar data file url
-        url = "https://mirrors.tuna.tsinghua.edu.cn/wikipedia-monthly"
-        resp = urllib.request.urlopen(url, timeout=60, cafile=certifi.where())
-        root = lxml.html.parse(resp)
-        for trElem in root.xpath(".//table[@id='list']/tbody/tr"):
-            aTag = trElem.xpath("./td")[0].xpath("./a")[0]
-            m = re.fullmatch("wikipedia-[0-9]+\\.tar", aTag.text)
-            if m is not None and (dstFile is None or dstFile < m.group(0)):
-                dstFile = os.path.join(dataDir, m.group(0))
-                dstFileUrl = os.path.join(url, aTag.get("href"))
-                dstMd5File = dstFile + ".md5"
-                dstMd5FileUrl = dstFileUrl + ".md5"
-        if dstFile is None:
-            raise Exception("no tar data file found")
-
-        # download md5 file
-        print("Download \"%s\"." % (dstMd5FileUrl))
-        _Util.wgetDownload(dstMd5FileUrl, dstMd5File)
-        sock.progress_changed(5)
-
-        # download data file
-        print("Download \"%s\"." % (dstFileUrl))
-        _Util.wgetDownload(dstFileUrl, dstFile)
-        if not _Util.verifyFile(dstFile, dstMd5File):
-            raise Exception("the downloaded file is corrupt")
-    sock.progress_changed(50)
-
-    # extract
-    # sometimes tar file contains minor errors
-    print("Extract \"%s\"." % (dstFile))
-    _Util.shellCallIgnoreResult("/bin/tar -x --strip-components=1 -C \"%s\" -f \"%s\"" % (dataDir, dstFile))
-    sock.progress_changed(60)
-
-    # sync
-    print("Synchonize.")
-    with _TempChdir(dataDir):
-        _Util.cmdExec("/usr/bin/repo", "sync")
-    sock.progress_changed(99)
-
-    # all done, delete the tar data file and md5 file
-    _Util.forceDelete(dstFile)
-    _Util.forceDelete(dstMd5File)
-    sock.progress_changed(100)
-
-
-def _update(dataDir, sock):
-    with _TempChdir(dataDir):
-        _Util.cmdExec("/usr/bin/repo", "sync")
+        # FIXME
+        # step 1: download dumps files
+        # step 2: auto newest import dumps files into mariadb
 
 
 class _Util:
-
-    @staticmethod
-    def deleteDirContent(path, fullfnIgnoreList=[]):
-        for fn in os.listdir(path):
-            fullfn = os.path.join(path, fn)
-            if fullfn in fullfnIgnoreList:
-                continue
-            _Util.forceDelete(fullfn)
-
-    @staticmethod
-    def readFile(filename):
-        with open(filename) as f:
-            return f.read()
-
-    @staticmethod
-    def verifyFile(filename, md5Filename):
-        with open(filename, "rb") as f:
-            thash = hashlib.md5()
-            while True:
-                block = f.read(65536)
-                if len(block) == 0:
-                    break
-                thash.update(block)
-            return thash.hexdigest() == _Util.readFile(md5Filename)
-
-    @staticmethod
-    def wgetDownload(url, localFile=None):
-        param = _Util.wgetCommonDownloadParam().split()
-        if localFile is None:
-            _Util.cmdExec("/usr/bin/wget", *param, url)
-        else:
-            _Util.cmdExec("/usr/bin/wget", *param, "-O", localFile, url)
-
-    @staticmethod
-    def wgetCommonDownloadParam():
-        return "-q --show-progress -t 0 -w 60 --random-wait -T 60 --passive-ftp"
-
-    @staticmethod
-    def forceDelete(filename):
-        if os.path.islink(filename):
-            os.remove(filename)
-        elif os.path.isfile(filename):
-            os.remove(filename)
-        elif os.path.isdir(filename):
-            shutil.rmtree(filename)
 
     @staticmethod
     def cmdExec(cmd, *kargs):
@@ -175,19 +59,6 @@ class _Util:
                              shell=True, universal_newlines=True)
         if ret.returncode > 128:
             time.sleep(1.0)
-
-
-class _TempChdir:
-
-    def __init__(self, dirname):
-        self.olddir = os.getcwd()
-        os.chdir(dirname)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        os.chdir(self.olddir)
 
 
 ###############################################################################
